fb7e7269166aca8dc993153b787a3aeefc30a268
Harsh Patil <itsharshhh6@gmail.com>
2025-06-10 07:33:36 +0530

    Managing multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import {useState} from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  
   const increment = () => {
-    setCount(count + 1);
+    setCount(count + step);
   }
   const decrement = () => {
-    setCount(count - 1);
+    setCount(count - step);
   }
   const reset = () => {
     setCount(0);
@@ -16,9 +19,33 @@ function App() {
   return (
     <div className="app-container">
       <h1>Counter Value : {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={decrement}>Decrement</button>
+      <input type='number'
+        value = {step}
+        onChange={(e) => setStep(parseInt(e.target.value))}
+        placeholder='Enter step value'
+        min='1'
+        max='100'
+        style={{width: '200px', padding: '10px', fontSize: '16px'}}
+        autoFocus
+        required
+        pattern="[0-9]*"
+        title="Please enter a valid number between 1 and 100"
+      />
+      <br />
+      <br />
+      <button onClick={increment}
+        style={{backgroundColor: 'green', color: 'white', padding: '10px 20px', fontSize: '16px'}}>
+      Increment</button>
+      <br />
+      <br />
+      <button onClick={reset} 
+        style={{backgroundColor: 'red', color: 'white', padding: '10px 20px', fontSize: '16px'}}
+      >Reset</button>
+      <br />
+      <br />
+      <button onClick={decrement}
+        style={{backgroundColor: 'blue', color: 'white', padding: '10px 20px', fontSize: '16px'}}
+      >Decrement</button>
     </div>
       
   )
